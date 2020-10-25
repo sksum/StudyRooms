f2da30e0a8f428e645324999671420948e7bd287
sksum <sakshammrigdgr8@gmail.com>
2020-10-25 17:35:56 +0530

    views

---
+++ client/src/Views/Conference.svelte
@@ -0,0 +1,22 @@
+<script>
+    import { onMount } from 'svelte';
+
+    import Video_camera from '../Components/Video_camera.svelte';
+    import Video_roll from '../Components/Video_roll.svelte';
+    import Video_pinned from '../Components/Video_pinned.svelte';
+
+    onMount (() => {
+
+    })
+
+
+
+</script>
+
+
+
+<div>
+    <Video_camera />
+    <Video_pinned />
+    <Video_roll />
+</div>
